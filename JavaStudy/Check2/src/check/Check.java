4f8ac2b01030c227403e26a544e7b81e9d9473d4
ShinichiAshiki <shicp74@gmail.com>
2020-10-22 15:35:02 +0900

    課題提出_チェックテスト2
    2章チェックテスト

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,22 @@
+package check;
+import static constants.Constants.*;
+
+public class Check {
+
+    private static String firstName = "新一";
+    private static String lastName = "足木";
+
+    public static void main(String[] args) {
+        System.out.print("printNameメソッド → ");
+        printName(lastName,firstName);
+
+        Pet pet = new Pet(CHECK_CLASS_JAVA, CHECK_CLASS_HOGE);
+        RobotPet robotPet = new RobotPet(CHECK_CLASS_R2D2, CHECK_CLASS_LUKE);
+
+        pet.introduce();
+        robotPet.introduce();
+    }
+    private static void printName(String firstName, String lastName) {
+        System.out.println(firstName + lastName);
+    }
+}
