87633e040a1cd7bd12ec9157a8d3d5e608525008
ShinichiAshiki <shicp74@gmail.com>
2020-10-30 18:51:10 +0900

    課題提出_チェックテスト2
    レビュー中コード変更

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -7,8 +7,7 @@ public class Check {
     private static String lastName = "足木";
 
     public static void main(String[] args) {
-        System.out.print("printNameメソッド → ");
-        printName(lastName,firstName);
+        System.out.println("printNameメソッド → " + printName(lastName,firstName));
 
         Pet pet = new Pet(CHECK_CLASS_JAVA, CHECK_CLASS_HOGE);
         RobotPet robotPet = new RobotPet(CHECK_CLASS_R2D2, CHECK_CLASS_LUKE);
@@ -16,7 +15,8 @@ public class Check {
         pet.introduce();
         robotPet.introduce();
     }
-    private static void printName(String firstName, String lastName) {
-        System.out.println(firstName + lastName);
+    private static String printName(String firstName, String lastName) {
+        return (firstName + lastName);
     }
+
 }
