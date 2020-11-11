343737ff24abe71846348ed84d649726cd655dbd
ShinichiAshiki <shicp74@gmail.com>
2020-11-11 23:05:52 +0900

    課題提出1_7
    クラスとインスタンス

---
+++ JavaScript/1-7/main.js
@@ -0,0 +1,9 @@
+class Taiyaki{
+    constructor(Nakami){
+        console.log('中身は' + Nakami + 'です')
+    }
+}
+
+let Nakami = new Taiyaki('あんこ');
+Nakami = new Taiyaki('クリーム');
+Nakami = new Taiyaki('チーズ');
