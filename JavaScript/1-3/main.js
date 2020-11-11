464f4bdc0a844df1fba592412d4f4c4ebb2b18cc
ShinichiAshiki <shicp74@gmail.com>
2020-11-11 22:33:20 +0900

    課題提出1_3
    条件分岐・演算子

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,21 @@
+//問1回答
+console.log('a != 10')
+console.log('b >= 10 && b < 20')
+
+//問2回答
+let x = 10;
+if ( x >= 10 && x <= 20 ) {
+    console.log('成功です');
+} else {
+    console.log('失敗です'); 
+}
+
+//問3回答
+let y = 0;
+if ( y % 2 === 0 ) {
+    console.log('偶数です');
+} else if ( y % 2 === 1 ) {
+    console.log('奇数です'); 
+} else {
+    //console.log('バカ除け')
+}
